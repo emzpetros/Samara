8e985902ae7382fdc6677642f3e326afffc4c9ed
Eileen PC <emzpetros@gmail.com>
2026-06-22 09:55:40 -0400

    music and sound management

---
+++ Assets/Scripts/SOs/AudioClipRefSO.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+[CreateAssetMenu()]
+public class AudioClipRefSO : ScriptableObject {
+    //public AudioClip attack;
+    public AudioClip windWhoosh;
+    public AudioClip gameOver;
+    public AudioClip warningBeep;
+    public AudioClip victory;
+    public AudioClip bump;
+    //public AudioClip success;
+    //public AudioClip failure;
+    //public AudioClip pickup;
+}
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance {  get; private set; }
+    [SerializeField] private AudioClipRefSO audioClipRefSO;
+
+    private PlayerController player;
+
+
+    private float volume = 1f;
+
+    private void Awake() {
+        Instance = this;
+    }
+
+
+    private void Start() {
+        player = PlayerController.Instance;
+
+        player.OnSpinStart += Player_OnSpinStart;
+    }
+
+    private void Player_OnSpinStart(object sender, EventArgs e) {
+        PlaySound(audioClipRefSO.windWhoosh, player.transform.position, 0.5f);
+    }
+
+    private void PlaySound(AudioClip audioClip, Vector3 pos, float volumeMult = 1f) {
+        AudioSource.PlayClipAtPoint(audioClip, pos, volumeMult * volume);
+    }
+
+}
